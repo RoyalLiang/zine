d34e7ee10ef2842539101b01ca1729dd179fc7dd
royalliang <royal.liang.459@gmail.com>
2021-03-04 22:13:20 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import home from './pages/home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    home
   }
 }
 </script>
